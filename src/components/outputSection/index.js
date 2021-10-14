396dde773afd82f56a7f78fe90748c36fffd6362
aj502gm <20004118@galileo.edu>
2021-10-14 11:14:30 -0600

    Iframe update still not working

--- src/components/outputSection/index.js
+++ src/components/outputSection/index.js
@@ -1,8 +1,16 @@
+import { useContext, useEffect } from "react";
+import { txtDataCT } from "../../context/txtAreaData/TxtDataCT";
 export default function OutPutTxt(){
+    const { htmlDataTemplate } = useContext(txtDataCT);
+    useEffect(() => {
+        const node = document.querySelector('#outputTxt');
+        node.innerHTML = {htmlDataTemplate}
+        //alert(node.innerHTML.value);
+    });
     return(
         <div className="outputBox border black-success border-5 m-1" id = "outDiv">
-            <iframe id = 'outputTxt' sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts">
-
+            <iframe id = 'outputTxt' dangerouslySetInnerHTML = {htmlDataTemplate} sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts" > 
+    
             </iframe>
         </div>
     )
