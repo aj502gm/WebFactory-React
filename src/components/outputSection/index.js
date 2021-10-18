f6de5c9b4416666b37ab5bf93f79b685cf0fa493
aj502gm <20004118@galileo.edu>
2021-10-18 11:18:21 -0600

    Iframe component is not working with dangerouslySetInnerHtml. A new solution could be a div (doesn't recognize JS script tag). Still figuring out an optimal solution

--- src/components/outputSection/index.js
+++ src/components/outputSection/index.js
@@ -1,17 +1,31 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { txtDataCT } from "../../context/txtAreaData/TxtDataCT";
 export default function OutPutTxt(){
     const { htmlDataTemplate } = useContext(txtDataCT);
-    useEffect(() => {
-        const node = document.querySelector('#outputTxt');
-        node.innerHTML = {htmlDataTemplate}
-        //alert(node.innerHTML.value);
-    });
-    return(
-        <div className="outputBox border black-success border-5 m-1" id = "outDiv">
-            <iframe id = 'outputTxt' dangerouslySetInnerHTML = {htmlDataTemplate} sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts" > 
-    
-            </iframe>
+    const htmlprueba = `<!doctype html>
+    <html lang="en"> 
+        <head> 
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style type="text/css"></style>
+        </head>
+        <body><h1>hola</h1><script></script></body>
+    </html>;`
+    //const [innertHtmlData, setInnerHtmlData] = useState({__html: htmlDataTemplate});
+    // useEffect(() => {
+    //     const node = document.querySelector('#outputTxt');
+    //     node.innerHTML = {__html: htmlDataTemplate}
+    //     //alert(node.innerHTML.value);
+    //     console.error({__html: htmlDataTemplate}.__html);
+    //     console.log()
+    // });
+    return (
+        <div className="outputBox border black-success border-5 m-1" id = "outDiv"  dangerouslySetInnerHTML = {{__html: htmlDataTemplate}}>
+           
         </div>
     )
 }
+
+/*
+https://codepen.io/jamesnimlos/pen/VKAaJO?editors=1010*/
+//<iframe id = 'outputTxt' sandbox='allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' dangerouslySetInnerHTML = ${htmlprueba}></iframe>
