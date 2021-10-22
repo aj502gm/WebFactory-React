8ffcd9358bffae7e7fa67979968c3bea07d70ce1
aj502gm <20004118@galileo.edu>
2021-10-22 09:51:24 -0600

    Jzip,File saver pending to fix. saveAs function is not defined.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import CodeEditor from './components/codeEditorSection/index';
 import Nav from "./components/nav/index";
 import OutPutTxt from "./components/outputSection/index";
 import {TxtDataProvider} from "./context/txtAreaData/TxtDataCT";
+// var FileSaver = require('file-saver');
+let zip = require('jszip')();
 function App() {
   return (
     <TxtDataProvider>
@@ -17,3 +19,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/nav/buttonSection/index.js
+++ src/components/nav/buttonSection/index.js
@@ -1,5 +1,12 @@
-import Buttons from "./Buttons"
+import Buttons from "./Buttons";
+import { txtDataCT } from "../../../context/txtAreaData/TxtDataCT";
+// import zip from './../../../App';
 export default function ButtonSection(){
+   
+    // const saveProject = () => {
+    //     zip.file("hello.txt", "Wassup");
+    //     zip.generateAsync({type: "blob"}).then( (files) => saveAs(files, "myProject.zip")); //.zip
+    // }
     return(
         <>
             <div className = "d-flex">
