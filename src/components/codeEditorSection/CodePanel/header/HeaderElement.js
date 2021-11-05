e07b8ae65972b5fcf6f812887cec4cabee3d7bbe
aj502gm <20004118@galileo.edu>
2021-11-05 12:11:59 -0600

    Uploading existing file functionality pending to do

--- src/components/codeEditorSection/CodePanel/header/HeaderElement.js
+++ src/components/codeEditorSection/CodePanel/header/HeaderElement.js
@@ -1,7 +1,23 @@
 export default function HeaderElement({labelValue, panelId, btnClassStyle}){
+    const mostrar = () => {
+//document.getElementById('fileInput').click();
+//document.getElementById('fileInput').click();
+        let input = document.createElement('input');
+        input.type = 'file';
+         input.accept = '.html';//(esto.id === 'htmlSelector')? '.html': (esto.id === 'cssSelector')? '.css' : ".js";
+        // let outputTxt = (esto.id === 'htmlSelector')? 'htmlTxt': (esto.id === 'cssSelector')? 'cssTxt' : "jsTxt";
+        input.onchange = _ => {
+            let files =   Array.from(input.files);
+            let reader = new FileReader();
+            reader.onload = () => document.getElementById('htmlSelector').value = reader.result;
+            reader.readAsText(files[0]);
+        }
+        document.getElementById('input').click();
+    }
     return(
         <>
-            <a href = "#" className={btnClassStyle} id ={panelId}>↑</a>
+            <input id="fileInput" type="file" style={{display:'none'}} />
+            <button  className={btnClassStyle} id ={panelId} onClick = {mostrar()}>↑</button>
             <h5 className = "m-2">{labelValue}</h5>
         </>
     )
