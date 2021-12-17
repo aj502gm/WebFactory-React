5a95d976ae79051b581e0d034c4cc8f5bbb4b182
aj502gm <20004118@galileo.edu>
2021-12-17 11:46:40 -0600

    Iframe still not working...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CodeEditor from './components/codeEditorSection/index';
 import Nav from "./components/nav/index";
-import OutPutTxt from "./components/outputSection/index";
 import {TxtDataProvider} from "./context/txtAreaData/TxtDataCT";
+import {useContext, useEffect} from "react";
+import { txtDataCT } from "./context/txtAreaData/TxtDataCT";
+import OutPutTxt from "./components/outputSection/index";
 // var FileSaver = require('file-saver');
 import FileSaver from 'file-saver';
 export let zip = require('jszip')();
 function App() {
+  const { htmlDataTemplate } = useContext(txtDataCT);
+  useEffect(()=>{
+   alert('montado'); 
+  }, [])
   return (
     <TxtDataProvider>
       <div className="App">
